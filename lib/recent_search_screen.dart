d261688d036b8b5755dc7cc165f072c9ba8a598b
Gopika G <gopika.g@robosoftin.com>
2024-08-11 22:29:15 +0530

    Added search screen and recent search screen

--- lib/recent_search_screen.dart
+++ lib/recent_search_screen.dart
@@ -1,16 +1,131 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class RecentSearchScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class RecentSearchScreen extends StatefulWidget {
   const RecentSearchScreen({super.key});
+
+  @override
+  _RecentSearchScreenState createState() => _RecentSearchScreenState();
+}
+
+class _RecentSearchScreenState extends State<RecentSearchScreen> {
+  List<Map<String, dynamic>> _recentSearches = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRecentSearches();
+  }
+
+  Future<void> _loadRecentSearches() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> recentSearchesJson =
+        prefs.getStringList('recent_searches') ?? [];
+
+    setState(() {
+      _recentSearches = recentSearchesJson
+          .map((item) =>
+              jsonDecode(item) as Map<String, dynamic>) // Explicit cast
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recent Search'),
+        title: const Text(
+          'Favourites',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: const Center(
-        child: Text('This is the recent search screen'),
+      body: Container(
+        color: const Color.fromRGBO(136, 81, 204, 0.68),
+        child: _recentSearches.isEmpty
+            ? const Center(
+                child: Text(
+                  'No favourite cities added',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            : ListView.builder(
+                itemCount: _recentSearches.length,
+                itemBuilder: (context, index) {
+                  if (_recentSearches[index].isEmpty) {
+                    return Container(); // Skip if there's an error in the data
+                  }
+
+                  String cityName =
+                      _recentSearches[index]['cityName'] ?? 'Unknown City';
+                  double temperature =
+                      _recentSearches[index]['temperature'] ?? 0.0;
+                  String description =
+                      _recentSearches[index]['description'] ?? 'No description';
+
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context, _recentSearches[index]);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                cityName,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  const Icon(
+                                    Icons.wb_sunny,
+                                    color: Color(0xFFFFA500),
+                                    size: 50.0,
+                                  ),
+                                  const SizedBox(width: 16.0),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        '${temperature.toStringAsFixed(1)}°C',
+                                        style: const TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        description,
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/weather_service.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -10,7 +13,6 @@ class _SearchScreen extends State<SearchScreen> {
   TextEditingController _controller = TextEditingController();
   List<String> _cities = [];
   final WeatherService _weatherService = WeatherService();
-
   bool _isLoading = false;
 
   Future<void> _searchCity(String query) async {
@@ -32,6 +34,29 @@ class _SearchScreen extends State<SearchScreen> {
     });
   }
 
+  Future<void> _saveRecentSearch(
+      String cityName, double temperature, String description) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> recentSearches = prefs.getStringList('recent_searches') ?? [];
+
+    Map<String, dynamic> cityData = {
+      'cityName': cityName,
+      'temperature': temperature,
+      'description': description,
+    };
+
+    String cityDataJson = jsonEncode(cityData);
+
+    // Avoid duplicates
+    recentSearches.removeWhere((item) {
+      Map<String, dynamic> data = jsonDecode(item);
+      return data['cityName'] == cityName;
+    });
+
+    recentSearches.add(cityDataJson);
+    await prefs.setStringList('recent_searches', recentSearches);
+  }
+
   @override
   void initState() {
     super.initState();
@@ -51,9 +76,8 @@ class _SearchScreen extends State<SearchScreen> {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(kToolbarHeight + 20),
-        // Adjust height for padding
         child: Container(
-          padding: const EdgeInsets.only(top: 20), // Add desired top padding
+          padding: const EdgeInsets.only(top: 20),
           child: AppBar(
             leading: IconButton(
               icon: const Icon(Icons.arrow_back),
@@ -81,8 +105,17 @@ class _SearchScreen extends State<SearchScreen> {
                 itemBuilder: (context, index) {
                   return ListTile(
                     title: Text(_cities[index]),
-                    onTap: () {
-                      // Handle city selection
+                    onTap: () async {
+                      // Fetch the weather data for the selected city
+                      final weatherData =
+                          await _weatherService.fetchWeather(_cities[index]);
+                      double temperature = weatherData['main']['temp'];
+                      String description = weatherData['weather'][0]['main'];
+
+                      // Save the city name, temperature, and description
+                      _saveRecentSearch(
+                          _cities[index], temperature, description);
+
                       Navigator.pop(context, _cities[index]);
                     },
                   );
